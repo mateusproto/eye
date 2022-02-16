fd11e0bdfed32bdb19ebaf79681184c027bd867e
Mateus Proto <66911264+mateusproto@users.noreply.github.com>
2022-02-16 13:41:04 -0300

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,9 +1,13 @@
+// Find the eyes
 const balls = document.getElementsByClassName('ball');
 
+// Detect move movement event
 document.onmousemove = (event) => {
+  // Calculate X and Y position
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
+  // Move the yes according to mouse position
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
